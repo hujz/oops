dcad7262eee3e055527b2923d5df3fd586fcb1d3
hujz <hujz@bingosoft.net>
2019-02-28 20:07:01 +0800

    scp

--- src/main.go
+++ src/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("hello golang")
+	client, err := OpenSSHSession("hujz", "123", "127.0.0.1:22")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	client.Stderr = os.Stdout
+	client.Stdout = os.Stdout
+
+	client.Run("scp --help")
 }
